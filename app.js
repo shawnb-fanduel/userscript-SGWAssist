fa580be42a229d9d9ec8fbc36d9212a8cf69b610
shawnb-fanduel <shawn.brooker@fanduel.com>
2024-01-31 08:32:43 -0800

    feat: v0.6.0

--- app.js
+++ app.js
@@ -1,29 +1,29 @@
 // ==UserScript==
 // @name         Highlight dates&times
 // @namespace    fanduel.com
-// @version      0.5.0
+// @version      0.6.0
 // @description  Highlights table cells in SGW
 // @author       Shawn Brooker
-// @match        http*://sgw.gcp-prod.tvg.com/ITOps/cardSummary/TvgRaceList?TVGCardID=*
-// @match        http*://sgw.gcp-dev.tvg.com/ITOps/cardSummary/TvgRaceList?TVGCardID=*
+// @match        http*://sgw.gcp-prod.tvg.com/ITOps/cardSummary/*RaceList?TVGCardID=*
+// @match        http*://sgw.gcp-dev.tvg.com/ITOps/cardSummary/*RaceList?TVGCardID=*
 // ==/UserScript==
 
 (function() {
 	'use strict';
 
-	const wrongDateColor = "#cca000"
-	const wrongTimeColor = "#cc6600"
+   const wrongDateColor = "#cca000"
+   const wrongTimeColor = "#cc6600"
 
 	console.log('Script started');
 	const tbody = document.querySelector('#datatable_TVG_Race_List tbody');
 	if (!tbody) {
-		console.log('Tbody not found, script exiting');
+		console.log('Tbody not found');
 		return;
 	}
-	const today = new Date();
-	const todayDate = today.toISOString().slice(0, 10);
 	const cardDate = document.querySelector('body > div.container-fluid > div:nth-child(1) > div > h6 > a:nth-child(1)').innerText.trim();
 	const rows = tbody.querySelectorAll("tr");
+	const today = new Date();
+	const todayDate = today.toISOString().slice(0, 10);
 	// console.log('Today\'s Date:', todayDate);
 
 	rows.forEach(row => {
@@ -38,7 +38,7 @@
 			const timeParts = timeString.split(':');
 			if (timeParts.length === 3) {
 				const hours = parseInt(timeParts[0], 10);
-				if (!isNaN(hours) && hours >= 1 && hours < 3) {
+				if (!isNaN(hours) && hours >= 1 && hours < 4) {
 					cell.style.backgroundColor = wrongTimeColor;
 				}
 			}
